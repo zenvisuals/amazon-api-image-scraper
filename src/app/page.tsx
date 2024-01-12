de02483fe242cbd71cf2ee31ba2682de1ea5e320
Chee Zhiquan <zq@zv.sg>
2024-01-12 01:00:55 +0000

    Initialize NextJS App

---
+++ src/app/page.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="w-1/2">
+        <label className="form-control w-full max-w-xs">
+          <div className="label">
+            <span className="label-text">Access Key</span>
+          </div>
+          <input
+            type="password"
+            placeholder="Type here"
+            className="input input-bordered w-full max-w-xs"
+          />
+        </label>
+        <label className="form-control w-full max-w-xs">
+          <div className="label">
+            <span className="label-text">Secret Key</span>
+          </div>
+          <input
+            type="password"
+            placeholder="Type here"
+            className="input input-bordered w-full max-w-xs"
+          />
+        </label>
+        <label className="form-control w-full max-w-xs">
+          <div className="label">
+            <span className="label-text">Amazon Product Link</span>
+          </div>
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full max-w-xs"
+          />
+        </label>
+      </div>
+    </main>
+  );
+}
